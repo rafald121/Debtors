93581cf63c7300ca893b8fd4285eef4f49a808fb
rafald121 <rafald121@gmail.com>
2017-02-11 14:57:52 +0100

    end transaction with payment// not tested

--- app/src/main/java/com/example/android/debtors/Logic/RealizeTransactionHelper.java
+++ app/src/main/java/com/example/android/debtors/Logic/RealizeTransactionHelper.java
@@ -4,8 +4,10 @@ import android.util.Log;
 
 import com.example.android.debtors.Model.Client;
 import com.example.android.debtors.Model.Owner;
+import com.example.android.debtors.Model.Payment;
 import com.example.android.debtors.Model.Transaction;
 import com.example.android.debtors.Model.TransactionForClient;
+import com.example.android.debtors.Utils.Utils;
 
 /**
  * Created by Rafaello on 2017-02-11.
@@ -21,34 +23,54 @@ public class RealizeTransactionHelper {
     }
 
     public void realizeTransaction(TransactionForClient transaction){
-        if(transaction==null)
-            Log.e(TAG, "realizeTransaction: TRANSACTION IS NULL");
-        else {
 
-            if(transaction.getTransactionEntryPayment()>0){
-                Log.i(TAG, "realizeTransaction: PODANO ENTRY PAYMENT" + transaction.getTransactionEntryPayment());
-            } else
-                Log.i(TAG, "realizeTransaction: NIE PODANO ENTRY PAYMENT" + transaction.getTransactionEntryPayment());
-
-            int amount = transaction.getTransactionQuantity();
-            int value = transaction.getProductValue();
-            int totalValue = amount * value;
-
-            boolean gotOrGive = transaction.isTransactionSeller();
+        if(transaction!=null){
 
             Owner owner = transaction.getTransactionOwner();
             Client client = transaction.getTransactionClient();
-            if(owner != null && client != null) {
-                changeOwnerValue(owner, totalValue, gotOrGive);
-                addTransactionToOwnerList(owner, transaction);
+            int amount = transaction.getTransactionQuantity();
+            int value = transaction.getProductValue();
+            int totalValue = amount * value;
+            boolean gotOrGive = transaction.isTransactionSeller();
 
-                changeClientValue(client, totalValue, gotOrGive); //jesli ostatnie true = Client wisi
-                // więcej
-                addTransactionToClientList(client, transaction);
-            } else{
+            if (owner != null && client != null) {
+
+                if (transaction.getTransactionEntryPayment() > 0) {
+                    Log.i(TAG, "realizeTransaction: PODANO ENTRY PAYMENT: " + transaction
+                            .getTransactionEntryPayment());
+                    Payment payment = new Payment(Utils.getDateTime(), owner, client, transaction.getTransactionEntryPayment(),
+                            gotOrGive);
+
+                    RealizePaymentHelper realizePaymentHelper = new RealizePaymentHelper();
+                    realizePaymentHelper.realizePayment(payment);
+
+                    changeOwnerValue(owner, totalValue, gotOrGive);
+                    addTransactionToOwnerList(owner, transaction);
+
+                    changeClientValue(client, totalValue, gotOrGive); //jesli ostatnie true = Client wisi
+                    // więcej
+                    addTransactionToClientList(client, transaction);
+
+                } else {
+
+                    Log.i(TAG, "realizeTransaction: NIE PODANO ENTRY PAYMENT: " + transaction
+                            .getTransactionEntryPayment());
+//                    TYLKO ZMIANA ZA TRANZAKCJE
+                    changeOwnerValue(owner, totalValue, gotOrGive);
+                    addTransactionToOwnerList(owner, transaction);
+
+                    changeClientValue(client, totalValue, gotOrGive); //jesli ostatnie true = Client wisi
+                    // więcej
+                    addTransactionToClientList(client, transaction);
+                }
+
+            } else {
                 Log.e(TAG, "realizeTransaction: CLIENT OR OWNER IS NULL");
             }
+        } else {
+            Log.e(TAG, "realizeTransaction: TRANSACTION IS NULL");
         }
+
     }
 
 
