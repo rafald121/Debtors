f6b77da00eebfa78ef6a4b0206d03dcc58e312a5
rafald121 <rafald121@gmail.com>
2017-02-15 16:35:18 +0100

    add dot and circle TEST!!

---
+++ app/src/main/java/com/example/android/debtors/Activities/ActivitySettings.java
@@ -0,0 +1,11 @@
+package com.example.android.debtors.Activities;
+
+/**
+ * Created by Rafaello on 2017-02-15.
+ */
+
+public class ActivitySettings {
+
+//    TODO np domyślne sortowanie, język ? :D, styl bleble
+
+}
