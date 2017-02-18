fe1a9fc9ebd2d3af9fe5b920e4f60325924348c9
rafald121 <rafald121@gmail.com>
2017-02-19 00:23:10 +0100

    two tabs show but doesn't respond

--- app/src/main/java/com/example/android/debtors/Fragments/FragmentDebtorsForMe.java
+++ app/src/main/java/com/example/android/debtors/Fragments/FragmentDebtorsForMe.java
@@ -1,5 +1,6 @@
 package com.example.android.debtors.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.android.debtors.R;
 
@@ -16,7 +18,21 @@ import com.example.android.debtors.R;
 public class FragmentDebtorsForMe extends Fragment{
     
     private static final String TAG = FragmentDebtorsForMe.class.getSimpleName();
-    
+
+
+    public FragmentDebtorsForMe() {
+        Log.i(TAG, "FragmentDebtors: START");
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate: start");
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "onCreate: end");
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,6 +42,18 @@ public class FragmentDebtorsForMe extends Fragment{
         return rootView;
 
     }
+    @Override
+    public void onAttach(Context context) {
+        Toast.makeText(context, "halo", Toast.LENGTH_SHORT).show();
 
+        Log.i(TAG, "onAttach: START");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i(TAG, "onDetach: START");
+        super.onDetach();
+    }
    
 }
--- app/src/main/java/com/example/android/debtors/Fragments/FragmentDebtorsMeToOther.java
+++ app/src/main/java/com/example/android/debtors/Fragments/FragmentDebtorsMeToOther.java
@@ -1,5 +1,6 @@
 package com.example.android.debtors.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.android.debtors.R;
 
@@ -17,6 +19,18 @@ public class FragmentDebtorsMeToOther extends Fragment {
 
     private static final String TAG = FragmentDebtorsMeToOther.class.getSimpleName();
 
+    public FragmentDebtorsMeToOther() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate: start");
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "onCreate: end");
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -24,5 +38,17 @@ public class FragmentDebtorsMeToOther extends Fragment {
         View rootView = inflater.inflate(R.layout.recycler, container, false);
         Log.i(TAG, "onCreateView: END");
         return rootView;
+    }
+    @Override
+    public void onAttach(Context context) {
+        Toast.makeText(context, "halo", Toast.LENGTH_SHORT).show();
+
+        Log.i(TAG, "onAttach: START");
+        super.onAttach(context);
+    }
+    @Override
+    public void onDetach() {
+        Log.i(TAG, "onDetach: START");
+        super.onDetach();
     }
 }
